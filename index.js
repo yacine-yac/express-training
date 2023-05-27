5c936b9728c34bf5d698d8ab5e07d457a4224e08
yacine <lentente1958@gmail.com>
2023-05-27 19:30:30 +0100

    send file method

--- index.js
+++ index.js
@@ -5,16 +5,36 @@ const express=require('express');
 const server=express();
 
 server.get('/api',(req,res,next)=>{
-   // served with any request (GET,POST,DELETE,PUT) 
-    console.log('ttttt'); 
+   // execute methods
+    console.log('ttttt');
+    // execute next to pass to the next handler 
     next();
-},(req,res)=>{res.send('second ....')})
+},
+(req,res)=>{
+    res.status(200).send('second ....')
+
+})
 
 server.get('/api/Search',(rq,rs)=>{
     // return query variables
     const query=rq.query;
     console.log(query)
     rs.send(`product name ${query?.name} `)
+})
+
+
+// use route method
+server.route("/book").get((req,res)=>{
+        res.send('Release books')
+}).post((req,res)=>{
+
+    Sres.send('create new book')
+
+})
+
+server.get('/profil',(req,res)=>{
+    //send file 
+    res.sendFile('/dir/profil.html',{root:__dirname})
 })
 
 // static files 
