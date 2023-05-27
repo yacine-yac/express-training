f3d17c1ba12089b0e2965e7b4fa6914669f7e18c
yacine <lentente1958@gmail.com>
2023-05-27 16:24:33 +0100

    static routes

--- index.js
+++ index.js
@@ -4,11 +4,10 @@ const express=require('express');
 // server return an object
 const server=express();
 
-// server endpoints
-
-// GET method
-server.get('/',(req,res)=>{res.send('Hello world')})
 
 
+// static files 
+server.use(express.static('./app'))
+// Routting
 // Port where the server is running
 server.listen(3000,(err)=>{console.log(err)})
